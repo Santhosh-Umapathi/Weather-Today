e335d659670416b1686da72771f5a33c5a1d0a12
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-05-23 13:09:12 +0200

    FEAT: refactor Close icon component and add Search icon component

--- src/components/icons/Close.tsx
+++ src/components/icons/Close.tsx
@@ -1,38 +1,18 @@
 import * as React from 'react';
-import Svg, {G, Mask, Path, Defs, ClipPath} from 'react-native-svg';
+import Svg, {Path} from 'react-native-svg';
 import type {SvgProps} from 'react-native-svg';
+import {colors} from '../../tokens';
 const SvgClose = (props: SvgProps) => (
   <Svg
-    width={props.width}
-    height={props.height}
+    width={props.width || 24}
+    height={props.height || 24}
     fill="none"
-    viewBox="0 0 16 16"
+    viewBox="0 0 12 12"
     {...props}>
-    <G clipPath="url(#close_svg__a)">
-      <Mask
-        id="close_svg__b"
-        width={16}
-        height={16}
-        x={0}
-        y={0}
-        maskUnits="userSpaceOnUse"
-        style={{
-          maskType: 'luminance',
-        }}>
-        <Path fill="none" d="M0 0h16v16H0z" />
-      </Mask>
-      <G mask="url(#close_svg__b)">
-        <Path
-          fill={props.color}
-          d="M12.2 3.807a.663.663 0 0 0-.94 0L8 7.06 4.74 3.8a.665.665 0 0 0-.94.94L7.06 8 3.8 11.26a.665.665 0 1 0 .94.94L8 8.94l3.26 3.26a.664.664 0 1 0 .94-.94L8.94 8l3.26-3.26a.67.67 0 0 0 0-.933"
-        />
-      </G>
-    </G>
-    <Defs>
-      <ClipPath id="close_svg__a">
-        <Path fill="none" d="M0 0h16v16H0z" />
-      </ClipPath>
-    </Defs>
+    <Path
+      fill={props.color || colors.black}
+      d="M6 6.7 3.55 9.15a.47.47 0 0 1-.35.137.47.47 0 0 1-.35-.137.47.47 0 0 1-.137-.35q0-.213.137-.35L5.3 6 2.85 3.55a.47.47 0 0 1-.137-.35q0-.212.137-.35a.47.47 0 0 1 .35-.138q.212 0 .35.138L6 5.3l2.45-2.45a.47.47 0 0 1 .35-.138q.212 0 .35.138a.47.47 0 0 1 .138.35.47.47 0 0 1-.138.35L6.7 6l2.45 2.45a.47.47 0 0 1 .138.35.47.47 0 0 1-.138.35.47.47 0 0 1-.35.137.47.47 0 0 1-.35-.137z"
+    />
   </Svg>
 );
 export default SvgClose;
--- src/components/icons/SearchIcon.tsx
+++ src/components/icons/SearchIcon.tsx
@@ -1,19 +1,20 @@
 import * as React from 'react';
 import Svg, {Path} from 'react-native-svg';
 import type {SvgProps} from 'react-native-svg';
-const SvgSearch = (props: SvgProps) => (
+import {colors} from '../../tokens';
+const SvgSearchIcon = (props: SvgProps) => (
   <Svg
-    width={props.width}
-    height={props.height}
+    width={props.width || 24}
+    height={props.height || 24}
     fill="none"
     viewBox="0 0 18 18"
     {...props}>
     <Path
-      stroke={props.color}
+      stroke={props.color || colors.black}
       strokeLinecap="round"
       strokeWidth={1.6}
       d="m14.796 14.584 1.697 1.697m-3.89-3.465a6.5 6.5 0 1 1-9.192-9.193 6.5 6.5 0 0 1 9.193 9.193Z"
     />
   </Svg>
 );
-export default SvgSearch;
+export default SvgSearchIcon;
