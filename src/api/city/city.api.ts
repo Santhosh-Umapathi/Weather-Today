b316aa33452f5eee7cf465caa4ff4a8ecd6ee2eb
Santhosh Umapathi <santhosh.professional1@gmail.com>
2025-05-29 13:55:02 +0200

    FIX: Mock cities added for API issue

--- src/api/city/city.api.ts
+++ src/api/city/city.api.ts
@@ -3,15 +3,16 @@ import {CITIES_URL} from '../../const';
 import {AXIOS_HEADERS} from '../../config';
 import {TCitiesBE} from './city.api.types';
 import {mapCitiesToDTO} from '../../dto';
+import MOCK_CITIES from './mockResponse.json';
 
 export const getCities = async () => {
   try {
-    const response = await axios.get(CITIES_URL, {
-      headers: AXIOS_HEADERS,
-    });
+    // const response = await axios.get(CITIES_URL, {
+    //   headers: AXIOS_HEADERS,
+    // });
 
     // Convert to Client DTO
-    const results = mapCitiesToDTO(response.data as TCitiesBE);
+    const results = mapCitiesToDTO(MOCK_CITIES as TCitiesBE);
 
     return results;
   } catch (error) {
